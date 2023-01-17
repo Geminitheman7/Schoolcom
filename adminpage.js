97a996ea4457de6fc387a5851fd33f602d3d715d
Anshuman405 <73084682+Anshuman405@users.noreply.github.com>
2023-01-16 17:59:51 -0800

    Update adminpage.js

--- adminpage.js
+++ adminpage.js
@@ -2,9 +2,9 @@ function login() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
   
-  if (username == "eagleadmin" && password == "adminsm417") {
-    alert("Login successful!");
+  if (username === "eagleadmin" && password === "adminsm417") {
+    window.location.href = "success.html";
   } else {
-    alert("Invalid username or password.");
+    window.location.href = "fail.html";
   }
 }
